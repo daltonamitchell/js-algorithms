86a56248cced81b7c694c7f2a888bad58c09beaa
Dalton A. Mitchell <daltonamitchell@live.com>
2016-05-26 09:52:00 -0700

    Refactor from classes to functions

--- spec/LetterCounterSpec.js
+++ spec/LetterCounterSpec.js
@@ -1,36 +1,31 @@
-import LetterCounter from '../lib/LetterCounter';
+import letterCount from '../lib/letter-count';
 
-describe('LetterCounter', () => {
-  describe('#count', () => {
-    it('handles a simple case', () => {
-      const counter = new LetterCounter();
-      const expected = { c: 1, a: 1, t: 1 };
+describe('letterCount', () => {
+  it('handles a simple case', () => {
+    const expected = { c: 1, a: 1, t: 1 };
 
-      expect(counter.count('cat')).toEqual(expected);
-    });
+    expect(letterCount('cat')).toEqual(expected);
+  });
 
-    it('handles multiples of the same letter', () => {
-      const counter = new LetterCounter();
-      const expected = { m: 1, o: 2, n: 1 };
+  it('handles multiples of the same letter', () => {
+    const expected = { m: 1, o: 2, n: 1 };
 
-      expect(counter.count('moon')).toEqual(expected);
-    });
+    expect(letterCount('moon')).toEqual(expected);
+  });
 
-    it('handles a multi-word case', () => {
-      const counter = new LetterCounter();
-      const expected = {
-        c: 1,
-        a: 2,
-        t: 1,
-        s: 1,
-        r: 1,
-        e: 1,
-        f: 1,
-        u: 1,
-        n: 1,
-      };
+  it('handles a multi-word case', () => {
+    const expected = {
+      c: 1,
+      a: 2,
+      t: 1,
+      s: 1,
+      r: 1,
+      e: 1,
+      f: 1,
+      u: 1,
+      n: 1,
+    };
 
-      expect(counter.count('cats are fun')).toEqual(expected);
-    });
+    expect(letterCount('cats are fun')).toEqual(expected);
   });
 });
